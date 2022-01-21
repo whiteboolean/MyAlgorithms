88021e8b12c1470de9746c3f00e640b8b30f91cf
guandi <whiteboolean@gmail.com>
2022-01-21 15:57:27 +0800

    将两个有序链表合并成一个有序链表

--- app/src/main/java/com/example/myalgorithms/algorithms/MergeSortedLinkedNode.java
+++ app/src/main/java/com/example/myalgorithms/algorithms/MergeSortedLinkedNode.java
@@ -8,42 +8,59 @@ public class MergeSortedLinkedNode {
     }
 
 
-    private static void mergeNode(LinkedNode node1,LinkedNode node2){
-
+    private static LinkedNode mergeNode(LinkedNode node1, LinkedNode node2) {
         //TEMP
         LinkedNode node = new LinkedNode(0);
 
-        //f
-        LinkedNode temp ;
+        //临时的头结点
+        LinkedNode temp = node;
+
+        while (node1 != null && node2 != null) {
+            if (node1.node < node2.node) {
+                temp.next = node1;
+                node1 = node1.next;
+            } else {
+                temp.next = node2;
+                node2 = node2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (node1 != null) {
+            temp.next = node1;
+        }
+        if (node2 != null) {
+            temp.next = node2;
+        }
+        return node.next;
 
 
     }
 
-    private static class ListNode{
-        public static LinkedNode head;
+    private static class ListNode {
+        public LinkedNode head;
 
         //尾插建立链表
-        public static void addLast(int data) {
+        public void addLast(int data) {
             LinkedNode node = new LinkedNode(data);
             if (head == null) {
                 this.head = node;
             } else {
-                ListNode cur = this.head;
+                LinkedNode cur = this.head;
                 while (cur.next != null) {
                     cur = cur.next;
                 }
                 cur.next = node;
             }
         }
-
     }
 
 
-    private static class LinkedNode{
-        public Object node;
-        public LinkedNode Next;
+    private static class LinkedNode {
+        public int node;
+        public LinkedNode next;
 
-        public LinkedNode(Object node) {
+        public LinkedNode(int node) {
             this.node = node;
         }
     }
