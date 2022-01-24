fb52b6baaa42275cfdc89f770f0077a6027dca11
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-01-24 23:39:02 +0800

    判断是否是回文数字

---
+++ app/src/main/java/com/example/myalgorithms/leetcode/LeetCode9.java
@@ -0,0 +1,45 @@
+package com.example.myalgorithms.leetcode;
+
+public class LeetCode9 {
+
+    public static void main(String[] args) {
+        boolean palindrome = isPalindrome(1000021);
+        System.out.println(palindrome);
+
+    }
+
+    public static boolean isPalindrome(int x) {
+//        boolean isPalindrome = false;
+//        if (x < 0) {
+//            return false;
+//        }else if (x>0 && x<10){
+//            return true;
+//        }
+//        String newX = String.valueOf(x);
+//        for (int i = 0; i < newX.length(); i++) {
+//            int last = newX.length() - i-1;
+//            char c = newX.charAt(i);
+//            char c1 = newX.charAt(last);
+//            if (i < last && c1 != 0) {
+//                isPalindrome = String.valueOf(c).equals(String.valueOf(c1));
+//                if (!isPalindrome) return false;
+//            }
+//        }
+//        return isPalindrome;
+
+
+        //利用StringBuilder的性质
+//        StringBuffer sb = new StringBuffer( x + "");
+//        sb.reverse();
+//        return sb.toString().equals(x + "");
+
+        if (x == 0) return true;
+        if (x < 0 || x % 10 == 0) return false;
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversed || x == reversed / 10;
+    }
+}
--- app/src/main/java/com/example/myalgorithms/leetcode/NumSum.java
+++ app/src/main/java/com/example/myalgorithms/leetcode/NumSum.java
@@ -1,13 +1,13 @@
 package com.example.myalgorithms.leetcode;
 
-import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NumSum {
 
     public static void main(String[] args) {
-
-        int[] array = {2, 7,2, 11, 15};
+        int[] array = {1, 2, 7, 11, 15, 2};
         int target = 4;
         System.out.println("原始数组:" + Arrays.toString(array));
         System.out.println("target:" + target);
@@ -19,15 +19,15 @@ public class NumSum {
     public static int[] twoSum(int[] nums, int target) {
         int[] indexs = new int[2];
         // 双重循环 循环极限为(n^2-n)/2
-        for(int i = 0; i < nums.length; i++){
-            for(int j = nums.length - 1; j > i; j --){
-                if(nums[i]+nums[j] == target){
-                    indexs[0] = i;
-                    indexs[1] = j;
-                    return indexs;
-                }
-            }
-        }
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = nums.length - 1; j > i; j--) {
+//                if (nums[i] + nums[j] == target) {
+//                    indexs[0] = i;
+//                    indexs[1] = j;
+//                    return indexs;
+//                }
+//            }
+//        }
 
         //2,7,11,15
         //1. 2,7 9
@@ -42,6 +42,15 @@ public class NumSum {
 //                }
 //            }
 //        }
+
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                indexs[0] = hashMap.get(nums[i]);
+                indexs[1] = i;
+            }
+            hashMap.put(target - nums[i], i);
+        }
 
         return indexs;
     }
