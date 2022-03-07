a7df69ab7c550a0751e3e56b63be18b2586f27b7
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-03-07 09:02:10 +0800

    贪心算法

---
+++ app/src/main/java/com/example/myalgorithms/bilibili_tuling/P20_最长连续递增序列.java
@@ -0,0 +1,30 @@
+package com.example.myalgorithms.bilibili_tuling;
+
+/**
+ * 最长连续递增序列
+ * <p>
+ * 给定一个未经排序的整数数组，找到最长且连续递增的子序列，
+ * 并返回该序列的长度
+ * 序列的下标是连续的
+ * 贪心算法
+ */
+public class P20_最长连续递增序列 {
+
+    public static void main(String[] args) {
+
+        System.out.println(findLength(new int[]{1, 2, 3, 2, 3, 4, 3, 4, 5, 6, 7}));
+    }
+
+    public static int findLength(int[] nums) {
+        int start = 0;
+        int max = 0 ;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i]<=nums[i-1]){
+                start = i ;
+            }
+            max = Math.max(i - start + 1,max);
+        }
+        return max ;
+    }
+}
